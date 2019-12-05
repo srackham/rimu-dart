58ff8ad8ca253a5ebf95a73c3c3aa850334bd523
Stuart Rackham <srackham@gmail.com>
2019-12-05 16:04:24 +1300

    Finished rimuc tests.

--- test/rimuc_test.dart
+++ test/rimuc_test.dart
@@ -1,8 +1,31 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:rimu/src/rimuc.dart';
 import 'package:test/test.dart';
 
 // Helpers.
+
+class TestSpec {
+  String unsupported;
+  String description;
+  String args;
+  String input;
+  String expectedOutput;
+  int exitCode;
+  String predicate;
+  bool layouts;
+
+  TestSpec.fromJson(dynamic decoded) {
+    this.unsupported = decoded['unsupported'] ?? '';
+    this.description = decoded['description'];
+    this.args = decoded['args'];
+    this.input = decoded['input'];
+    this.expectedOutput = decoded['expectedOutput'];
+    this.exitCode = decoded['exitCode'] ?? 0;
+    this.predicate = decoded['predicate'];
+    this.layouts = decoded['layouts'] ?? false;
+  }
+}
 
 // Execute rimuc.dart in command shell.
 // args: rimuc command args.
@@ -38,5 +61,56 @@ void main() {
     expect(result.exitCode, 1);
     expect(result.stderr.toString().startsWith('illegal --layout: foobar'),
         isTrue);
+  });
+
+  // Execute test cases specified in JSON file rimuc-tests.json
+  test('jsonTests', () {
+    final jsonSource = File('./test/rimuc-tests.json').readAsStringSync();
+    final decoded = json.decode(jsonSource);
+    for (var d in decoded) {
+      var spec = TestSpec.fromJson(d);
+
+      // TODO: Drop this stub.
+      print(spec.description);
+      continue;
+
+      if (spec.unsupported.contains('dart')) {
+        continue;
+      }
+      for (var layout in ['', 'classic', 'flex', 'sequel']) {
+        // Skip if not a layouts test and we have a layout, or if it is a layouts test but no layout is specified.
+        if (!spec.layouts && layout.isNotEmpty ||
+            spec.layouts && layout.isEmpty) {
+          continue;
+        }
+        var args = spec.args.replaceAll('./src/examples/example-rimurc.rmu',
+            './test/fixtures/example-rimurc.rmu');
+        if (layout.isNotEmpty) {
+          args = '--layout $layout $args';
+        }
+        var result = execRimuc(args: args, input: spec.input);
+        var output = '${result.stdout}${result.stderr}';
+        expect(result.exitCode, spec.exitCode);
+        switch (spec.predicate) {
+          case "equals":
+            expect(output == spec.expectedOutput, isTrue);
+            break;
+          case "!equals":
+            expect(output != spec.expectedOutput, isTrue);
+            break;
+          case "contains":
+            expect(output.contains(spec.expectedOutput), isTrue);
+            break;
+          case "!contains":
+            expect(!output.contains(spec.expectedOutput), isTrue);
+            break;
+          case "startsWith":
+            expect(output.startsWith(spec.expectedOutput), isTrue);
+            break;
+          default:
+            throw (spec.description + ': illegal predicate: ' + spec.predicate);
+        }
+      }
+    }
   });
 }
