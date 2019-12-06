db292630d7448cd5ac88d7ddb98ce4f370112103
Stuart Rackham <srackham@gmail.com>
2019-12-06 15:24:07 +1300

    Implemented expansion.dart and expansioin_test.dart

---
+++ lib/src/expansion.dart
@@ -0,0 +1,67 @@
+import 'options.dart' as options;
+
+class ExpansionOptions {
+  // Processing priority (highest to lowest): container, skip, spans and specials.
+  // If spans is true then both spans and specials are processed.
+  // They are assumed false if they are not explicitly defined.
+  // If a custom filter is specified their use depends on the filter.
+  bool macros;
+  bool container;
+  bool skip;
+  bool spans; // Span substitution also expands special characters.
+  bool specials;
+
+  ExpansionOptions(
+      {this.macros, this.container, this.skip, this.spans, this.specials});
+
+  bool operator ==(Object other) =>
+      other is ExpansionOptions &&
+      macros == other.macros &&
+      container == other.container &&
+      skip == other.skip &&
+      spans == other.spans &&
+      specials == other.specials;
+
+  void merge(ExpansionOptions from) {
+    this.macros = from.macros ?? this.macros;
+    this.container = from.container ?? this.container;
+    this.skip = from.skip ?? this.skip;
+    this.spans = from.spans ?? this.spans;
+    this.specials = from.specials ?? this.specials;
+  }
+
+  // Parse block-options string into blockOptions.
+  void parse(String opts) {
+    if (opts.isNotEmpty) {
+      for (var opt in opts.trim().split(RegExp(r'\s+'))) {
+        if (options.isSafeModeNz() && opt == '-specials') {
+          options.errorCallback('-specials block option not valid in safeMode');
+          continue;
+        }
+        if (RegExp(r'^[+-](macros|spans|specials|container|skip)$')
+            .hasMatch(opt)) {
+          var value = opt[0] == '+';
+          switch (opt.substring(1)) {
+            case 'macros':
+              this.macros = value;
+              break;
+            case 'spans':
+              this.spans = value;
+              break;
+            case 'specials':
+              this.specials = value;
+              break;
+            case 'container':
+              this.container = value;
+              break;
+            case 'skip':
+              this.skip = value;
+              break;
+          }
+        } else {
+          options.errorCallback('illegal block option: ' + opt);
+        }
+      }
+    }
+  }
+}
---
+++ test/expansion_test.dart
@@ -0,0 +1,30 @@
+// Basic rendering tests (full syntax tested in rimu_test.dart).
+
+import 'package:test/test.dart';
+import 'package:rimu/src/expansion.dart';
+
+void main() {
+  test('expasionOptions', () {
+    var opts = ExpansionOptions(macros: true, specials: false);
+    opts.merge(ExpansionOptions(macros: false, container: true));
+    expect(
+        opts,
+        ExpansionOptions(
+            macros: false,
+            container: true,
+            skip: null,
+            spans: null,
+            specials: false));
+
+    opts = ExpansionOptions(macros: true, specials: false);
+    opts.parse("-macros +spans");
+    expect(
+        opts,
+        ExpansionOptions(
+            macros: false,
+            container: null,
+            skip: null,
+            spans: true,
+            specials: false));
+  });
+}
