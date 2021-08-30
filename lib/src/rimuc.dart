8b9e51f37f4670989a66f93513c40f6d9a9559ce
Stuart Rackham <srackham@gmail.com>
2021-08-30 14:19:29 +1200

    bump version number

--- lib/src/rimuc.dart
+++ lib/src/rimuc.dart
@@ -10,7 +10,7 @@ import 'resources.dart';
   rimuc app implementation.
 */
 
-const VERSION = '11.1.7';
+const VERSION = '11.1.8';
 final String HOME_DIR =
     Platform.environment[Platform.isWindows ? 'UserProfile' : 'HOME'];
 final String RIMURC = path.join(HOME_DIR, '.rimurc');
